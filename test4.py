24dcb97762e37b1e7c8ceeac84ef15f7d40423ec
sunshuailong <1101126130@qq.com>
2020-07-29 21:48:54 +0800

    1

--- test4.py
+++ test4.py
@@ -1,7 +1,5 @@
-def fibs(n):
-    res =[0,1]
-    for i in range(n-2):
-        res.append(res[-2]+res[-1])
-    return res
 
-print(dir(fibs))
+def my_digit():
+    return 1,2,3
+now =my_digit()
+print(now)

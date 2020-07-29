6ba2f4527de5da0d41ad4423b824b00aeb559e89
sunshuailong <1101126130@qq.com>
2020-07-29 14:08:25 +0800

    1

--- test4.py
+++ test4.py
@@ -1,2 +1,9 @@
-with open('new.txt','a') as f :
-    f.write('/hello,world')
+my_list = ['r','o','c','k','y']
+
+my_iter=iter(my_list)
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
+print(my_iter.__next__())
